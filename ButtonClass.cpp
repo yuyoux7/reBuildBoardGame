68e574ed95d688581ec497279d59bc472773a7f2
宇佑 蕭 <yuyoux7@gmail.com>
2024-11-30 13:32:52 +0800

    Write Button lib

--- ButtonClass.cpp
+++ ButtonClass.cpp
@@ -1,16 +1,46 @@
 #include "ButtonClass.h"
 
-bool ButtonClass::GetRegisterData(void) const
+bool ButtonClass::GetRegisterData(void)
 {
+    AppDataRegister* File = new AppDataRegister;
+    this->Data = File->AppDataSent(TYPE_IMG, "Button");
+    delete File;
+    if (this->Data)
+    {
+        return true;
+    }
     return false;
 }
 
 bool ButtonClass::UnRegisterData(void) const
 {
+    if (this->Data)
+    {
+        NULL >> this->Data;
+        return true;
+    }
     return false;
+}
+
+void ButtonClass::SetScenes(string Scenes)
+{
+    this->Scenes = Scenes;
+}
+
+void ButtonClass::ButtonProcess(int width_local /*key in*/, int height_local /*key in*/, string ID)
+{
+    AppDataProcess* FlashData = new AppDataProcess;
+    this->ID = this->Data[ID];
+    this->hlocal = FlashData->DisplayHeight(this->ID);
+    this->wlocal = FlashData->DisplayWidth(this->ID);
+    delete FlashData;
 }
 
 bool ButtonClass::ButtonState(string ID) const
 {
-    return false;
+    if (this->ID == ID)
+    {
+        return this->stat;
+    }
+    return NULL;
 }
--- ButtonClass.h
+++ ButtonClass.h
@@ -1,19 +1,24 @@
 #pragma once
 #include "ExampleH.h"
-#include "AppDataRegister.h"
+#include "AppDataProcess.h"
 struct ButtonData
 {
 	unsigned int Width{};
 	unsigned int Height{};
 	unsigned int wlocal{};	//to simple left
 	unsigned int hlocal{};	//to simple top
+	string Scenes{};
 	string ID{};
+	json Data{};
+	bool stat;
 };
 class ButtonClass : ButtonData
 {
 public:
-	bool GetRegisterData(void) const;
+	bool GetRegisterData(void);
 	bool UnRegisterData(void) const;
+	void SetScenes(string Scenes);
+	void ButtonProcess(int width_local,int height_local, string ID);
 	bool ButtonState(string ID) const;
 };
 
