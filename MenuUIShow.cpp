c27e69fcf048fba109300a3f0bd5bc391f54d55a
宇佑 蕭 <yuyoux7@gmail.com>
2024-12-11 21:12:12 +0800

    UpDate Menu

--- MenuUIShow.cpp
+++ MenuUIShow.cpp
@@ -8,26 +8,77 @@ MenuUIShow::MenuUIShow()
 	this->WindowSet = AppData->AppDataSent(TYPE_CFG, "WindowSize");
 	UI->WinUICreat(this->WindowSet["Width"], this->WindowSet["Height"], this->WindowSet["CmdShow"]);
 	UI->WinUITitleSet("BoardGame");
-	UI->SetClass("BackGround");
 	UI->SetScenes("Home");
+	UI->SetClass("BackGround");
 	UI->PutIMG("HomeBG");
-	this->PlayrTotal = 10;
+	UI->SetClass("Box");
+	UI->PutIMG("PlayerTotalBox");
 }
 
 void MenuUIShow::ScenesPlayerDataLoad(void)
 {
+	UI->SetScenes("PlayerDataLoad");
+	UI->SetClass("BackGround");
+	UI->PutIMG("OtherBG");
+	UI->SetClass("Box");
+	UI->PutIMG("SetNameBox");
+	UI->PutIMG("ExistValueBox");
+	UI->PutIMG("IntellectValueBox");
+	UI->PutIMG("AnchoredValueBox");
+	UI->PutIMG("EffectValueBox");
+	UI->PutIMG("UnderstandValueBox");
+	UI->PutIMG("ObservatuonValueBox");
+	UI->PutIMG("FourDiceValueBox");
+	UI->PutIMG("SixDiceValueBox");
 }
 
 void MenuUIShow::ScenesGameRotateDisplay(void)
 {
+	UI->SetScenes("GameRotateDisplay");
+	UI->SetClass("BackGround");
+	UI->PutIMG("OtherBG");
+	UI->SetClass("Text");
+	UI->PutIMG("NextPlayer");
+	UI->PutIMG("PlayerOrder");
+	UI->SetClass("Box");
+	UI->PutIMG("NowRoundBox");
 }
 
 void MenuUIShow::ScenesCardFunctionUsing(void)
 {
+	UI->SetScenes("CardFunctionUsing");
+	UI->SetClass("BackGround");
+	UI->PutIMG("OtherBG");
+	UI->SetClass("Box");
+	UI->PutIMG("NameBox");
+	UI->PutIMG("ExistValueBox");
+	UI->PutIMG("IntellectValueBox");
+	UI->PutIMG("AnchoredValueBox");
+	UI->PutIMG("EffectValueBox");
+	UI->PutIMG("UnderstandValueBox");
+	UI->PutIMG("ObservatuonValueBox");
+	UI->PutIMG("HaveEffectBox");
+	UI->PutIMG("NowRoundBox");
 }
 
 void MenuUIShow::ScenesAttack(void)
 {
+	UI->SetScenes("Attack");
+	UI->SetClass("BackGround");
+	UI->PutIMG("OtherBG");
+	UI->SetClass("Box");
+	UI->PutIMG("NameBox");
+	UI->PutIMG("ExistValueBox");
+	UI->PutIMG("IntellectValueBox");
+	UI->PutIMG("AnchoredValueBox");
+	UI->PutIMG("EffectValueBox");
+	UI->PutIMG("UnderstandValueBox");
+	UI->PutIMG("ObservatuonValueBox");
+	UI->PutIMG("TagEffectBox");
+	UI->PutIMG("AttackEffectBox");
+	UI->PutIMG("NowRoundBox");
+	UI->SetClass("Text");
+	UI->PutIMG("PlayerAttack");
 }
 
 int MenuUIShow::GetPlayerTotal(void) const
